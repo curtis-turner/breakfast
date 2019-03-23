2032ef93cf51daaa408f52e3e270813cb8296ada
Curtis Turner <cjturner714@gmail.com>
2019-03-23 14:02:04 -0700

    change breakfast method name to count_cals to better describe what the methods purpose is

--- breakfast/breakfast.py
+++ breakfast/breakfast.py
@@ -1,14 +1,18 @@
 class Breakfast():
-    def __init__(self, meal):
-        self.meal = meal
 
-    def calories(self):
+    def __init__(self, items):
+        self.items = items
+
+    def count_cals(self):
         cal_count = 0
-        for i in self.meal:
+        for i in self.items:
             cal_count += i.cals
 
         return cal_count
     
     def complete_breakfast(self):
-        for i in self.meal:
+        for i in self.items:
             print('{item} is a part of a complete and balanced breakfast.'.format(item=i.name))
+
+if __name__ =='__main__':
+    pass
